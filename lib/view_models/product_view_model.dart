649b123b0a539570cb52abd36b982aaeeba1bb3c
ola.sobeh <o.a.sobeh@gmail.com>
2021-11-22 21:04:14 +0200

    commit

--- lib/view_models/product_view_model.dart
+++ lib/view_models/product_view_model.dart
@@ -1,5 +1,5 @@
 import 'package:e_commere/model/AllProductsResponse.dart';
-import 'package:e_commere/services/Api_Service.dart';
+import 'package:e_commere/services/api_services.dart';
 import 'package:flutter/material.dart';
 
 class ProductViewModel extends ChangeNotifier{
